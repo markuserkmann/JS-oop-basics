9632cf527ee0ef77b2e224bb6191ad7e5ed49a64
Markus Erkmann <markus.erkmann@voco.ee>
2025-01-08 13:41:27 +0200

    Finished the grade calculation and the student/person class

--- index.js
+++ index.js
@@ -1,68 +1,13 @@
-const Person = require('./classes/person')
-const Student = require('./classes/student')
-const Course = require('./classes/course')
-const School = require('./classes/school')
+const Student = require('./classes/student.js');
 
-const school = new School("Awesome School")
-const student1 = new Student("John Smith")
-student1.setDateOfBirth(1995)
-const student2 = new Student("Mary Lee")
-student2.setDateOfBirth(2000)
-
-school.addStudent(student1)
-school.addStudent(student2)
-
-//we cannot add one student twice
-school.addStudent(student1)
-
-console.log(school.getStudents().length)  // 2
-
-const course1 = new Course("Math")
-const course2 = new Course("Physics")
-
-school.addCourse(course1)
-school.addCourse(course2)
-
-console.log(school)
-
-//we cannot add one course twice
-school.addCourse(course1)
-
-console.log(school.getCourses().length) // 2
-
-school.addStudentGrade(student1, course1, 4)
-school.addStudentGrade(student1, course2, 5)
-const Person = require('./classes/Person')
-const Student = require('./classes/Student')
-const Course = require('./classes/Course')
-const School = require('./classes/School')
-
-const school = new School("Awesome School")
-const student1 = new Student("John Smith")
-student1.setDateOfBirth(1995)
-const student2 = new Student("Mary Lee")
-student2.setDateOfBirth(2000)
-
-school.addStudent(student1)
-school.addStudent(student2)
-
-//we cannot add one student twice
-school.addStudent(student1)
-
-console.log(school.getStudents().length)  // 2
-
-const course1 = new Course("Math")
-const course2 = new Course("Physics")
-
-school.addCourse(course1)
-school.addCourse(course2)
-
-console.log(school)
-
-//we cannot add one course twice
-school.addCourse(course1)
-
-console.log(school.getCourses().length) // 2
-
-school.addStudentGrade(student1, course1, 4)
-school.addStudentGrade(student1, course2, 5)
+let ST = new Student("Markus Erkmann");
+// Age format: YearMonthDay
+ST.setDateOfBirth("20080226")
+ST.setId(12)
+ST.addGrade(1)
+ST.addGrade(4)
+ST.addGrade(5)
+console.log("Average grade: ", ST.getAverageGrade())
+console.log("Student grades: ", ST.getGrades())
+console.log("Student id: ", ST.getId())
+console.log(ST.getDescription())
